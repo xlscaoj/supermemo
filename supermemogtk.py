7ebf33d42a4c78ae8efda3be86d34cffddc22d95
Cao Jin <xlscaoj@gmail.com>
2014-01-13 23:45:08 +0800

    add score function

--- supermemogtk.py
+++ supermemogtk.py
@@ -12,44 +12,6 @@ class Mainboard:
     def destroy(self, widget, data=None):
         gtk.main_quit()
 
-    def show(self, widget):
-        return 'this is a test string'
-
-    def filp(self, widget, card_list):
-        #pdb.set_trace()
-        if self.current_card_side == 'front':
-            self.textbuffer.set_text('Answer:\n' + card_list[self.current_card_index].answer)
-            self.current_card_side = 'back'
-        elif self.current_card_side == 'back':
-            self.textbuffer.set_text('Question:\n' + card_list[self.current_card_index].question)
-            self.current_card_side = 'front'
-        else:
-            print 'argument of card_side has error'
-        
-        self.textview.show()
-        #self.scrolled_window.show()
-
-
-    def set_card_index(self, widget, command, len_card_list):
-        if command == 'prev':
-            self.current_card_index -= 1
-            self.current_card_index %= len_card_list
-        elif command == 'next':
-            self.current_card_index += 1
-            self.current_card_index %= len_card_list
-        else:
-            print 'navigate argument error'
-
-        if self.current_card_side == 'front':
-            self.textbuffer.set_text('Question:\n' + self.committed_list[self.current_card_index].question)
-        elif self.current_card_side == 'back':
-            self.textbuffer.set_text('Answer:\n' + self.committed_list[self.current_card_index].answer)
-        else:
-            print 'current_card_side error'
-
-        self.textview.show()
-
-
     def quit_test(self, widget, data=None):
         self.window.remove(self.test_box)
         self.window.add(self.mainbox)
@@ -58,8 +20,66 @@ class Mainboard:
         self.window.remove(self.study_box)
         self.window.add(self.mainbox)
 
+    def filp(self, widget, card):
+        #pdb.set_trace()
+        if self.current_card_side == 'front':
+            self.textbuffer.set_text('Answer:\n' + card.answer)
+            self.current_card_side = 'back'
+        elif self.current_card_side == 'back':
+            self.textbuffer.set_text('Question:\n' + card.question)
+            self.current_card_side = 'front'
+        else:
+            print 'argument of card_side has error'
+        
+        self.textview.show()
+        #self.scrolled_window.show()
+
+
+    def set_card_index(self, widget, command):
+        if len(self.test_list) == 0:
+            self.textbuffer.set_text("no card")
+        else:
+            if command == 'prev':
+                self.current_card_index -= 1
+                #if len(self.test_list) > 0:
+                self.current_card_index %= len(self.test_list)
+            elif command == 'next':
+                self.current_card_index += 1
+                self.current_card_index %= len(self.test_list)
+            else:
+                print 'navigate argument error'
+
+            self.textbuffer.set_text('Question:\n' + self.test_list[self.current_card_index].question)
+            self.current_card_side = 'front'
+        ## the following code may be used in browse mode
+        # if self.current_card_side == 'front':
+        #     self.textbuffer.set_text('Question:\n' + self.test_list[self.current_card_index].question)
+        # elif self.current_card_side == 'back':
+        #     self.textbuffer.set_text('Answer:\n' + self.test_list[self.current_card_index].answer)
+        # else:
+        #     print 'current_card_side error'
+
+
+    def evalute_card(self, widget, score):
+        if len(self.test_list) > 0:
+            card = self.test_list[self.current_card_index]
+            card.update(score)
+            self.test_list.remove(card)
+            if score < 3:
+                self.study_list.append(card)
+            self.current_card_index += 1
+            #pdb.set_trace()
+            if len(self.test_list) == 0:
+                self.textbuffer.set_text('No card, today')
+            else:
+                self.current_card_index %= len(self.test_list)
+                self.textbuffer.set_text('Question:\n' + self.test_list[self.current_card_index].question)
+
 
     def test(self, widget, data=None):
+        for card in self.committed_list:
+            if date.fromordinal(1) < card.next_date <= self.today:
+                self.test_list.append(card)
 
         self.test_box = gtk.VBox(False, 0)
         
@@ -73,7 +93,10 @@ class Mainboard:
         self.textview.set_justification(gtk.JUSTIFY_CENTER)
         self.textbuffer = self.textview.get_buffer()
 
-        self.textbuffer.set_text('Question:\n' + self.committed_list[self.current_card_index].question)
+        if len(self.test_list) == 0:
+            self.textbuffer.set_text('No card to be tested today')
+        else:
+            self.textbuffer.set_text('Question:\n' + self.test_list[self.current_card_index].question)
 
         self.textview.show()
 
@@ -91,40 +114,48 @@ class Mainboard:
         separator.show()
         
         button_show_answer = gtk.Button("Show Answer/Question")
-        button_show_answer.connect("clicked", self.filp, self.committed_list)
+        button_show_answer.connect("clicked", self.filp, self.test_list[self.current_card_index])
         button_show_answer.show()
 
         
         box_evaluate = gtk.HBox(False, 0)
-        score_button_1 = gtk.Button("0")
-        score_button_2 = gtk.Button("1")
-        score_button_3 = gtk.Button("2")
-        score_button_4 = gtk.Button("3")
-        score_button_5 = gtk.Button("4")
-        score_button_6 = gtk.Button("5")
+        score_button_0 = gtk.Button("0")
+        score_button_1 = gtk.Button("1")
+        score_button_2 = gtk.Button("2")
+        score_button_3 = gtk.Button("3")
+        score_button_4 = gtk.Button("4")
+        score_button_5 = gtk.Button("5")
         
+        score_button_0.connect("clicked", self.evalute_card, 0)
+        score_button_1.connect("clicked", self.evalute_card, 1)
+        score_button_2.connect("clicked", self.evalute_card, 2)
+        score_button_3.connect("clicked", self.evalute_card, 3)
+        score_button_4.connect("clicked", self.evalute_card, 4)
+        score_button_5.connect("clicked", self.evalute_card, 5)
+
+
+        score_button_0.show()
         score_button_1.show()
         score_button_2.show()
         score_button_3.show()
         score_button_4.show()
         score_button_5.show()
-        score_button_6.show()
         
+        box_evaluate.pack_start(score_button_0, False, True, 1)
         box_evaluate.pack_start(score_button_1, False, True, 1)
         box_evaluate.pack_start(score_button_2, False, True, 1)
         box_evaluate.pack_start(score_button_3, False, True, 1)
         box_evaluate.pack_start(score_button_4, False, True, 1)
         box_evaluate.pack_start(score_button_5, False, True, 1)
-        box_evaluate.pack_start(score_button_6, False, True, 1)
 
         box_evaluate.show()
 
         previous_card_button = gtk.Button("Previous")
-        previous_card_button.connect("clicked", self.set_card_index, 'prev', self.len_committed_list)
+        previous_card_button.connect("clicked", self.set_card_index, 'prev')
         previous_card_button.show()
 
         next_card_button = gtk.Button("Next")
-        next_card_button.connect("clicked", self.set_card_index, 'next', self.len_committed_list)
+        next_card_button.connect("clicked", self.set_card_index, 'next')
         next_card_button.show()
 
         quit_test_button = gtk.Button("Quit Test")
@@ -179,7 +210,7 @@ class Mainboard:
         separator.show()
 
         button_show_answer = gtk.Button("Show Answer/Question")
-        button_show_answer.connect("clicked", self.filp, self.all_card_list) #change to 'self.study_list' later
+        button_show_answer.connect("clicked", self.filp, self.study_list) #change to 'self.study_list' later
         button_show_answer.show()
         
         box_evaluate = gtk.HBox(False, 0)
@@ -226,11 +257,14 @@ class Mainboard:
         
 
     def __init__(self, all_card_list):
+        self.today = date.today()
+
         self.current_card_index = 0 # used for indicate the element in card_list
         self.current_card_side = 'front' # 'front'(question) or 'back'(answer)
 
         self.all_card_list = all_card_list
         [self.committed_list, self.uncommitted_list] = superfunc.seperate_committed_uncommitted(all_card_list)
+        self.test_list = []
         self.study_list = []
 
         self.len_committed_list = len(self.committed_list)
